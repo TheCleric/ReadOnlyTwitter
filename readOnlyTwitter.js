2798304aa676808afc4a54ffa98624ead5de96fc
Adam Weeden <adam.weeden@nrccua.org>
2020-08-21 11:43:15 -0400

    Initial commit of 1.0.1 version

---
+++ readOnlyTwitter.js
@@ -0,0 +1,137 @@
+const makeReadOnly = (options) => {
+    showHideNodesQuery(options.disableTweets, '[data-testid="SideNav_NewTweet_Button"', true);
+    showHideNodesQuery(options.disableComments, '[aria-label*="Compose a reply"]', true);
+    showHideNodesQuery(options.disableDirectMessages, '[data-testid="AppTabBar_DirectMessage_Link"');
+    showHideNodesQuery(options.disableDirectMessages, '[data-testid="DMDrawer"]');
+    showHideNodesQuery(options.disableRetweetsWithComments, '[href="/compose/tweet"][role="menuitem"]', false);
+
+    const tweetButtonInline = document.querySelectorAll('[data-testid="tweetButtonInline"');
+    if (tweetButtonInline && tweetButtonInline.length > 0) {
+        const rootParent = tweetButtonInline[0].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
+        showHideNodes(options.disableTweets, [rootParent]);
+    }
+
+    const shareViaDM = document.evaluate('//span[text()="Send via Direct Message"]', document).iterateNext();
+    if (shareViaDM) {
+        showHideNodes(options.disableDirectMessages, [shareViaDM.parentNode.parentNode.parentNode]);
+    }
+
+    enableDisableTwitterButton(options.disableComments, '[data-testid="reply"]');
+    enableDisableTwitterButton(options.disableRetweets, '[data-testid="retweet"]');
+    enableDisableTwitterButton(options.disableLikes, '[data-testid="like"]');
+};
+
+const enableDisableTwitterButton = (disable, domQuery) => {
+    if (disable) {
+        disableTwitterButton(domQuery);
+    } else {
+        enableTwitterButton(domQuery);
+    }
+}
+
+const disableTwitterButton = (domQuery) => {
+    const buttons = document.querySelectorAll(domQuery);
+    buttons && Array.from(buttons).forEach(button => {
+        if (!button.classList.contains('cloned') && !button.classList.contains('clone')) {
+            const cloned = button.cloneNode(true);
+            cloned.removeAttribute('href');
+            cloned.setAttribute('ignore', 'true');
+            if (cloned.childElementCount > 0) {
+                deepStyle(cloned.children[0], 'color', '#555555');
+            }
+            cloned.classList.add(['clone']);
+            button.classList.add(['cloned']);
+            button.setAttribute('ignore', 'true');
+            showHideNodes(true, [button]);
+            button.parentNode.appendChild(cloned);
+        }
+    });
+};
+
+const enableTwitterButton = (domQuery) => {
+    const buttons = document.querySelectorAll(domQuery);
+    buttons && Array.from(buttons).forEach(button => {
+        if (button.classList.contains('clone')) {
+            button.remove();
+        } else if (button.style.display === "none") {
+            button.classList.remove('cloned');
+            showHideNodes(false, [button]);
+        }
+    });
+}
+
+const deepStyle = (node, property, value) => {
+    node.style.setProperty(property, value);
+    if (node.childElementCount > 0) {
+        Array.from(node.children).forEach(child => {
+            deepStyle(child, property, value);
+        });
+    }
+}
+
+const showHideNodesQuery = (hide, domQuery, atParentLevel) => {
+    const nodes = Array.from(document.querySelectorAll(domQuery)).map(node => atParentLevel ? node.parentNode : node);
+    showHideNodes(hide, nodes);
+};
+
+const showHideNodes = (hide, nodes) => {
+    if (hide) {
+        nodes.forEach(node => { node.style.display = 'none'; });
+    } else {
+        nodes.forEach(node => { node.style.removeProperty('display'); });
+    }
+}
+
+const main = (options) => {
+    if (window.readOnlyTwitter && window.readOnlyTwitter.observer) {
+        window.readOnlyTwitter.observer.disconnect();
+    }
+
+    makeReadOnly(options);
+
+    const bodyNode = document.getElementsByTagName("body")[0];
+
+    const mutationCallback = function (mutationsList, observer) {
+        mutationsList.forEach(mutation => {
+            const toIgnore = (mutation.type === 'attributes')
+                || (mutation.type === 'childList' && mutation.removedNodes && mutation.removedNodes.length > 0)
+                || (mutation.type === 'childList' && mutation.addedNodes && Array.from(mutation.addedNodes).filter(x => x.getAttribute('ignore') === 'true').length > 0);
+            if (!toIgnore) {
+                makeReadOnly(options);
+            }
+        });
+    };
+
+    window.readOnlyTwitter = window.readOnlyTwitter || {};
+
+    window.readOnlyTwitter.observer = new MutationObserver(mutationCallback);
+
+    window.readOnlyTwitter.observer.observe(bodyNode, { attributes: true, childList: true, subtree: true });
+}
+
+
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+    for (var key in changes) {
+        if (key === "options") {
+            main(changes[key].newValue);
+        }
+    }
+});
+
+
+chrome.storage.local.get(['options'], (result) => {
+    let options = {
+        disableComments: true,
+        disableLikes: true,
+        disableRetweets: true,
+        disableRetweetsWithComments: true,
+        disableTweets: true,
+        disableDirectMessages: true,
+    };
+
+    if (result.options) {
+        options = result.options;
+    }
+
+    main(options);
+});
---
+++ readOnlyTwitterOptions.js
@@ -0,0 +1,53 @@
+const defaultOptions = {
+    disableComments: true,
+    disableLikes: true,
+    disableRetweets: true,
+    disableRetweetsWithComments: true,
+    disableTweets: true,
+    disableDirectMessages: true,
+};
+
+const addSwitchOption = (id, value, label, callback) => {
+    var template = document.getElementById('switch-options-template').innerHTML;
+    var rendered = Mustache.render(template, { id, value, label, checked: value ? 'checked' : '' });
+
+    const soNode = document.createElement("div");
+    soNode.innerHTML = rendered;
+    document.getElementById("options").appendChild(soNode);
+
+    document.getElementById(id).addEventListener('change', callback);
+}
+
+const updateOption = (option, value) => {
+    chrome.storage.local.get(['options'], (result) => {
+        let options = defaultOptions;
+
+        if (result.options) {
+            options = result.options;
+        }
+
+        options[option] = value;
+        chrome.storage.local.set({ options }, (result) => { });
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    chrome.storage.local.get(['options'], (result) => {
+        let options = defaultOptions;
+
+        if (result.options) {
+            options = result.options;
+        }
+
+        addSwitchOption("disable-comments", options.disableComments, "Disable Comments", (evt) => { updateOption('disableComments', evt.target.checked); });
+        addSwitchOption("disable-likes", options.disableLikes, "Disable Likes", (evt) => { updateOption('disableLikes', evt.target.checked); });
+        addSwitchOption("disable-retweets", options.disableRetweets, "Disable Retweets", (evt) => { updateOption('disableRetweets', evt.target.checked); });
+        addSwitchOption("disable-retweets-with-comments", options.disableRetweetsWithComments, "Disable Retweets With Comments", (evt) => { updateOption('disableRetweetsWithComments', evt.target.checked); });
+        addSwitchOption("disable-tweets", options.disableTweets, "Disable Tweets", (evt) => { updateOption('disableTweets', evt.target.checked); });
+        addSwitchOption("disable-dms", options.disableDirectMessages, "Disable Direct Messages", (evt) => { updateOption('disableDirectMessages', evt.target.checked); });
+
+        Array.from(document.querySelector('.mdc-switch') || []).forEach(sw => {
+            new mdc.switchControl.MDCSwitch(sw);
+        });
+    });
+});
